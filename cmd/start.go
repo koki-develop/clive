8b5415628b61e4e79435cc21a32815590da7f51a
koki-develop <kou.pg.0131@gmail.com>
2022-11-01 07:03:25 +0900

    move main process to start.go

--- cmd/start.go
+++ cmd/start.go
@@ -2,15 +2,83 @@ package cmd
 
 import (
 	"fmt"
+	"time"
 
+	"github.com/eiannone/keyboard"
 	"github.com/spf13/cobra"
 )
 
 var startCmd = &cobra.Command{
 	Use:   "start",
 	Short: "", // TODO
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("start called")
+	RunE: func(cmd *cobra.Command, args []string) error {
+		cfg, err := loadConfig("clive.yml")
+		if err != nil {
+			return err
+		}
+
+		port, err := randomUnusedPort()
+		if err != nil {
+			return err
+		}
+
+		ttyd := ttyd(port)
+		if err := ttyd.Start(); err != nil {
+			return err
+		}
+		defer ttyd.Process.Kill()
+
+		browser, err := launchBrowser()
+		if err != nil {
+			return err
+		}
+
+		page := browser.
+			NoDefaultDevice().
+			MustPage(fmt.Sprintf("http://localhost:%d", port)).
+			MustWaitIdle()
+
+		_ = page.MustEval("() => term.options.fontSize = 22")
+		_ = page.MustEval("term.fit")
+
+		for i, action := range cfg.Actions {
+			switch action := action.(type) {
+			case *typeAction:
+				fmt.Println(action.String())
+				for _, c := range action.Type {
+					_ = page.MustElement("textarea").Input(string(c))
+					_ = page.MustWaitIdle()
+					time.Sleep(action.Time)
+				}
+			case *keyAction:
+				fmt.Println(action.String())
+				for i := 0; i < action.Count; i++ {
+					_ = page.Keyboard.MustType(action.Key)
+					time.Sleep(action.Time)
+				}
+			case *pauseAction:
+				next := "quit"
+				if i+1 < len(cfg.Actions) {
+					next = cfg.Actions[i+1].String()
+				}
+				fmt.Printf("%s (next: %s)\n", action.String(), next)
+
+				for {
+					_, key, err := keyboard.GetSingleKey()
+					if err != nil {
+						return err
+					}
+					if key == keyboard.KeyEnter {
+						break
+					}
+				}
+			case *sleepAction:
+				fmt.Println(action.String())
+				time.Sleep(action.Time)
+			}
+		}
+
+		return nil
 	},
 }
 
