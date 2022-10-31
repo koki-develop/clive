2b17570a056c8f4093895d06d2bd4e18dc9e2c1a
koki-develop <kou.pg.0131@gmail.com>
2022-10-31 14:51:55 +0900

    implemented helper.RandomUnusedPort

--- cmd/root.go
+++ cmd/root.go
@@ -1,16 +1,15 @@
 /*
 Copyright © 2022 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
 import (
+	"fmt"
 	"os"
 
+	"github.com/koki-develop/clive/pkg/helper"
 	"github.com/spf13/cobra"
 )
-
-
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
@@ -25,6 +24,16 @@ to quickly create a Cobra application.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
+	RunE: func(cmd *cobra.Command, args []string) error {
+		port, err := helper.RandomUnusedPort()
+		if err != nil {
+			return err
+		}
+
+		fmt.Printf("port: %d\n", port)
+
+		return nil
+	},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -47,5 +56,3 @@ func init() {
 	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
-
-
---
+++ pkg/helper/cli.go
@@ -0,0 +1,18 @@
+package helper
+
+import (
+	"net"
+)
+
+func RandomUnusedPort() (int, error) {
+	addr, err := net.Listen("tcp", ":0")
+	if err != nil {
+		return 0, err
+	}
+
+	if err = addr.Close(); err != nil {
+		return 0, err
+	}
+
+	return addr.Addr().(*net.TCPAddr).Port, nil
+}
