aaba202323ad9ce7c653aafaa972c33216d6a980
koki-develop <kou.pg.0131@gmail.com>
2022-11-01 07:17:51 +0900

    implement init subcommand

--- cmd/config.go
+++ cmd/config.go
@@ -1,5 +1,7 @@
 package cmd
 
+const defaultConfigPath = "./clive.yml"
+
 type configYaml struct {
 	Actions []interface{}
 }
--- cmd/init.go
+++ cmd/init.go
@@ -1,16 +1,44 @@
 package cmd
 
 import (
+	"errors"
 	"fmt"
+	"os"
 
 	"github.com/spf13/cobra"
 )
 
+const configInitTemplate = `actions:
+  - pause
+  - type: echo 'Welcome to clive!'
+  - key: enter
+  - pause
+`
+
 var initCmd = &cobra.Command{
 	Use:   "init",
 	Short: "", // TODO
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("init called")
+	RunE: func(cmd *cobra.Command, args []string) error {
+		if _, err := os.Stat(defaultConfigPath); err != nil {
+			if !errors.Is(err, os.ErrNotExist) {
+				return err
+			}
+
+			f, err := os.Create(defaultConfigPath)
+			if err != nil {
+				return err
+			}
+			defer f.Close()
+
+			if _, err := f.Write([]byte(configInitTemplate)); err != nil {
+				return err
+			}
+
+			fmt.Printf("created %s\n", defaultConfigPath)
+			return nil
+		}
+
+		return fmt.Errorf("%s already exists", defaultConfigPath)
 	},
 }
 
