7697abab4588f52634fc7c2819696798e28a30b1
koki-develop <kou.pg.0131@gmail.com>
2022-11-01 05:09:51 +0900

    moved ttyd to ttyd.go

--- cmd/cli.go
+++ cmd/cli.go
@@ -1,9 +1,7 @@
 package cmd
 
 import (
-	"fmt"
 	"net"
-	"os/exec"
 )
 
 func randomUnusedPort() (int, error) {
@@ -17,17 +15,4 @@ func randomUnusedPort() (int, error) {
 	}
 
 	return addr.Addr().(*net.TCPAddr).Port, nil
-}
-
-func ttyd(port int) *exec.Cmd {
-	args := []string{
-		fmt.Sprintf("--port=%d", port),
-		"-t", "rendererType=canvas",
-		"-t", "disableResizeOverlay=true",
-		"-t", "cursorBlink=true",
-		"-t", "customGlyphs=true",
-		"bash", "--login",
-	}
-
-	return exec.Command("ttyd", args...)
 }
---
+++ cmd/ttyd.go
@@ -0,0 +1,19 @@
+package cmd
+
+import (
+	"fmt"
+	"os/exec"
+)
+
+func ttyd(port int) *exec.Cmd {
+	args := []string{
+		fmt.Sprintf("--port=%d", port),
+		"-t", "rendererType=canvas",
+		"-t", "disableResizeOverlay=true",
+		"-t", "cursorBlink=true",
+		"-t", "customGlyphs=true",
+		"bash", "--login",
+	}
+
+	return exec.Command("ttyd", args...)
+}
