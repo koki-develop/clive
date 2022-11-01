ada9237bb0d40efe74aaf3359c647c6e606345fb
koki-develop <kou.pg.0131@gmail.com>
2022-11-02 05:07:55 +0900

    move loadConfig to config.go

--- cmd/action.go
+++ cmd/action.go
@@ -2,13 +2,11 @@ package cmd
 
 import (
 	"fmt"
-	"os"
 	"strings"
 	"time"
 	"unicode/utf8"
 
 	"github.com/go-rod/rod/lib/input"
-	"gopkg.in/yaml.v3"
 )
 
 type action interface {
@@ -79,32 +77,6 @@ type ctrlAction struct {
 
 func (action *ctrlAction) String() string {
 	return fmt.Sprintf("Ctrl+%s", action.Ctrl)
-}
-
-func loadConfig(p string) (*config, error) {
-	f, err := os.Open(p)
-	if err != nil {
-		return nil, err
-	}
-	defer f.Close()
-
-	var y configYaml
-	if err := yaml.NewDecoder(f).Decode(&y); err != nil {
-		return nil, err
-	}
-
-	var actions []action
-	for _, a := range y.Actions {
-		action, err := parseAction(a)
-		if err != nil {
-			return nil, err
-		}
-		actions = append(actions, action)
-	}
-
-	return &config{
-		Actions: actions,
-	}, nil
 }
 
 func parseAction(v interface{}) (action, error) {
--- cmd/config.go
+++ cmd/config.go
@@ -1,5 +1,11 @@
 package cmd
 
+import (
+	"os"
+
+	"gopkg.in/yaml.v3"
+)
+
 const defaultConfigPath = "./clive.yml"
 
 type configYaml struct {
@@ -8,4 +14,30 @@ type configYaml struct {
 
 type config struct {
 	Actions []action
+}
+
+func loadConfig(p string) (*config, error) {
+	f, err := os.Open(p)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+
+	var y configYaml
+	if err := yaml.NewDecoder(f).Decode(&y); err != nil {
+		return nil, err
+	}
+
+	var actions []action
+	for _, a := range y.Actions {
+		action, err := parseAction(a)
+		if err != nil {
+			return nil, err
+		}
+		actions = append(actions, action)
+	}
+
+	return &config{
+		Actions: actions,
+	}, nil
 }
