aff10d0e5f8e3fd244167134fc29ee8346a04e37
koki-develop <kou.pg.0131@gmail.com>
2022-11-02 21:01:34 +0900

    add unit test for max

--- cmd/util_test.go
+++ cmd/util_test.go
@@ -48,3 +48,41 @@ func Test_truncateString(t *testing.T) {
 		})
 	}
 }
+
+func Test_max(t *testing.T) {
+	type args struct {
+		x int
+		y int
+	}
+	tests := []struct {
+		args args
+		want int
+	}{
+		{
+			args{0, 0},
+			0,
+		},
+		{
+			args{1, 0},
+			1,
+		},
+		{
+			args{0, 1},
+			1,
+		},
+		{
+			args{-1, 0},
+			0,
+		},
+		{
+			args{0, -1},
+			0,
+		},
+	}
+	for i, tt := range tests {
+		t.Run(fmt.Sprintf("#%d", i), func(t *testing.T) {
+			got := max(tt.args.x, tt.args.y)
+			assert.Equal(t, got, tt.want)
+		})
+	}
+}
