7ae007093264f756ca9df01dc6d0ccdc2ba6b306
koki-develop <kou.pg.0131@gmail.com>
2022-11-03 16:00:55 +0900

    implement unit test for parseKeyAction

--- cmd/action_test.go
+++ cmd/action_test.go
@@ -48,7 +48,57 @@ func Test_parseTypeAction(t *testing.T) {
 	for i, tt := range tests {
 		t.Run(fmt.Sprintf("#%d", i), func(t *testing.T) {
 			got, err := parseTypeAction(tt.args.m)
+			if tt.wantErr {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+			}
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
 
+func Test_parseKeyAction(t *testing.T) {
+	type args struct {
+		m map[string]interface{}
+	}
+	tests := []struct {
+		args    args
+		want    *keyAction
+		wantErr bool
+	}{
+		{
+			args{
+				map[string]interface{}{
+					"key": "enter",
+				},
+			},
+			&keyAction{
+				Key:   "enter",
+				Count: 1,
+				Speed: 10,
+			},
+			false,
+		},
+		{
+			args{
+				map[string]interface{}{
+					"key":   "enter",
+					"count": 10,
+					"speed": 500,
+				},
+			},
+			&keyAction{
+				Key:   "enter",
+				Count: 10,
+				Speed: 500,
+			},
+			false,
+		},
+	}
+	for i, tt := range tests {
+		t.Run(fmt.Sprintf("#%d", i), func(t *testing.T) {
+			got, err := parseKeyAction(tt.args.m)
 			if tt.wantErr {
 				assert.Error(t, err)
 			} else {
