dc11e98fa599192c7850f5eeb3cb63e275aeb9e8
koki-develop <kou.pg.0131@gmail.com>
2022-11-13 19:10:01 +0900

    handle spinner and ctrl+c

--- pkg/ui/update.go
+++ pkg/ui/update.go
@@ -1,8 +1,25 @@
 package ui
 
-import tea "github.com/charmbracelet/bubbletea"
+import (
+	"github.com/charmbracelet/bubbles/spinner"
+	tea "github.com/charmbracelet/bubbletea"
+)
 
 // TODO: implement
 func (m *Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	// spinner
+	case spinner.TickMsg:
+		var cmd tea.Cmd
+		m.spinner, cmd = m.spinner.Update(msg)
+		return m, cmd
+		// key
+	case tea.KeyMsg:
+		switch msg.Type {
+		case tea.KeyCtrlC:
+			return m, tea.Quit
+		}
+	}
+
 	return m, nil
 }
