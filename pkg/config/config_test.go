d8ec8e55a19e4949ac8780bb80f6d3b8b764890b
koki-develop <kou.pg.0131@gmail.com>
2022-11-15 20:45:27 +0900

    add unit test for DecodeMap

--- pkg/config/config_test.go
+++ pkg/config/config_test.go
@@ -40,9 +40,9 @@ actions:
 settings:
   loginCommand: ["hoge", "fuga"]
   fontSize: 100
-  fontFamily: FontName
+  fontFamily: FONT_FAMILY
   defaultSpeed: 200
-  browserBin: /path/to/browser
+  browserBin: BROWSER_BIN
 actions:
   - pause
 `,
@@ -50,9 +50,9 @@ actions:
 				Settings: &Settings{
 					LoginCommand: []string{"hoge", "fuga"},
 					FontSize:     100,
-					FontFamily:   util.String("FontName"),
+					FontFamily:   util.String("FONT_FAMILY"),
 					DefaultSpeed: 200,
-					BrowserBin:   util.String("/path/to/browser"),
+					BrowserBin:   util.String("BROWSER_BIN"),
 				},
 				Actions: []Action{
 					&PauseAction{},
@@ -134,6 +134,125 @@ actions:
 		t.Run(fmt.Sprintf("#%d", i), func(t *testing.T) {
 			got, err := Decode(strings.NewReader(tt.yaml))
 
+			if tt.wantErr {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+			}
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
+
+func TestDecodeMap(t *testing.T) {
+	tests := []struct {
+		input   map[string]interface{}
+		want    *Config
+		wantErr bool
+	}{
+		{
+			map[string]interface{}{
+				"actions": []interface{}{"pause"},
+			},
+			&Config{
+				Settings: &Settings{
+					LoginCommand: []string{"bash", "--login"},
+					FontSize:     22,
+					FontFamily:   nil,
+					DefaultSpeed: 10,
+					BrowserBin:   nil,
+				},
+				Actions: []Action{
+					&PauseAction{},
+				},
+			},
+			false,
+		},
+		{
+			map[string]interface{}{
+				"settings": map[string]interface{}{
+					"loginCommand": []interface{}{"hoge", "fuga"},
+					"fontSize":     100,
+					"fontFamily":   "FONT_FAMILY",
+					"defaultSpeed": 200,
+					"browserBin":   "BROWSER_BIN",
+				},
+				"actions": []interface{}{"pause"},
+			},
+			&Config{
+				Settings: &Settings{
+					LoginCommand: []string{"hoge", "fuga"},
+					FontSize:     100,
+					FontFamily:   util.String("FONT_FAMILY"),
+					DefaultSpeed: 200,
+					BrowserBin:   util.String("BROWSER_BIN"),
+				},
+				Actions: []Action{
+					&PauseAction{},
+				},
+			},
+			false,
+		},
+		{
+			map[string]interface{}{
+				"actions": []interface{}{
+					map[string]interface{}{"type": "Hello"},
+					map[string]interface{}{"type": "Hello", "count": 10, "speed": 500},
+					map[string]interface{}{"key": "enter"},
+					map[string]interface{}{"key": "enter", "count": 10, "speed": 500},
+					map[string]interface{}{"sleep": 1000},
+					"pause",
+					map[string]interface{}{"pause": nil},
+					map[string]interface{}{"ctrl": "c"},
+					map[string]interface{}{"ctrl": "c", "count": 10, "speed": 500},
+				},
+			},
+			&Config{
+				Settings: &Settings{
+					LoginCommand: []string{"bash", "--login"},
+					FontSize:     22,
+					FontFamily:   nil,
+					DefaultSpeed: 10,
+					BrowserBin:   nil,
+				},
+				Actions: []Action{
+					&TypeAction{Type: "Hello", Count: 1, Speed: 10},
+					&TypeAction{Type: "Hello", Count: 10, Speed: 500},
+					&KeyAction{Key: "enter", Count: 1, Speed: 10},
+					&KeyAction{Key: "enter", Count: 10, Speed: 500},
+					&SleepAction{Sleep: 1000},
+					&PauseAction{},
+					&PauseAction{},
+					&CtrlAction{Ctrl: "c", Count: 1, Speed: 10},
+					&CtrlAction{Ctrl: "c", Count: 10, Speed: 500},
+				},
+			},
+			false,
+		},
+		{
+			map[string]interface{}{"a": "A", "actions": []interface{}{"pause"}},
+			nil,
+			true,
+		},
+		{
+			map[string]interface{}{"settings": "hello world"},
+			nil,
+			true,
+		},
+		{
+			map[string]interface{}{"actions": "hello world"},
+			nil,
+			true,
+		},
+		{
+			map[string]interface{}{"actions": []interface{}{map[string]interface{}{"type": "hello world", "unknownField": "value"}}},
+			nil,
+			true,
+		},
+	}
+	for i, tt := range tests {
+		t.Run(fmt.Sprintf("#%d", i), func(t *testing.T) {
+			got, err := DecodeMap(tt.input)
 			if tt.wantErr {
 				assert.Error(t, err)
 			} else {
