af3069a1b9850da9350c522ab09984f2de6aadb8
koki-develop <kou.pg.0131@gmail.com>
2022-11-16 09:02:58 +0900

    create pkg/browser

--- pkg/ui/browser.go
+++ pkg/ui/browser.go
@@ -2,63 +2,35 @@ package ui
 
 import (
 	"fmt"
-	"time"
 
 	"github.com/go-rod/rod"
-	"github.com/go-rod/rod/lib/launcher"
-	"github.com/go-rod/rod/lib/proto"
+	"github.com/koki-develop/clive/pkg/browser"
 	"github.com/koki-develop/clive/pkg/config"
 )
 
 func openPage(cfg *config.Config, port int) (*rod.Page, error) {
-	browser, err := launchBrowser(cfg)
+	url := fmt.Sprintf("http://localhost:%d", port)
+	p, err := browser.Open(cfg.Settings.BrowserBin, url)
 	if err != nil {
 		return nil, err
 	}
 
-	page, err := browser.Page(proto.TargetCreateTarget{URL: fmt.Sprintf("http://localhost:%d", port)})
-	if err != nil {
-		return nil, err
-	}
-	if err := page.WaitIdle(time.Minute); err != nil {
-		return nil, err
-	}
-	if err := setupPage(cfg, page); err != nil {
+	if err := setupPage(cfg, p); err != nil {
 		return nil, err
 	}
 
-	return page, nil
-}
-
-func launchBrowser(cfg *config.Config) (*rod.Browser, error) {
-	path, _ := launcher.LookPath()
-	if cfg.Settings.BrowserBin != nil {
-		path = *cfg.Settings.BrowserBin
-	}
-
-	u, err := launcher.New().
-		Leakless(true).
-		Headless(false).
-		Bin(path).
-		Launch()
-	if err != nil {
-		return nil, err
-	}
-
-	browser := rod.New().ControlURL(u).NoDefaultDevice()
-	if err := browser.Connect(); err != nil {
-		return nil, err
-	}
-
-	return browser, nil
+	return p, nil
 }
 
 func setupPage(cfg *config.Config, page *rod.Page) error {
+	// font family
 	if cfg.Settings.FontFamily != nil {
 		if _, err := page.Eval(fmt.Sprintf("() => term.options.fontFamily = '%s'", *cfg.Settings.FontFamily)); err != nil {
 			return err
 		}
 	}
+
+	// font size
 	if _, err := page.Eval(fmt.Sprintf("() => term.options.fontSize = %d", cfg.Settings.FontSize)); err != nil {
 		return err
 	}
