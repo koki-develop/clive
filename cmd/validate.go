a19640a867ba6a8d4edc948ca0704830f09850f3
koki-develop <kou.pg.0131@gmail.com>
2022-11-16 13:39:53 +0900

    remove comments

--- cmd/validate.go
+++ cmd/validate.go
@@ -1,7 +1,3 @@
-/*
-Copyright © 2022 NAME HERE <EMAIL ADDRESS>
-
-*/
 package cmd
 
 import (
@@ -10,7 +6,6 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// validateCmd represents the validate command
 var validateCmd = &cobra.Command{
 	Use:   "validate",
 	Short: "A brief description of your command",
@@ -27,14 +22,4 @@ to quickly create a Cobra application.`,
 
 func init() {
 	rootCmd.AddCommand(validateCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// validateCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// validateCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
