6dc1f208cf78a5012b0c1bccdaa364f277d6e19a
koki-develop <kou.pg.0131@gmail.com>
2022-11-21 20:37:40 +0900

    create NetListener

--- pkg/util/net.go
+++ pkg/util/net.go
@@ -16,3 +16,32 @@ func RandomUnusedTCPPort() (int, error) {
 
 	return addr.Addr().(*net.TCPAddr).Port, nil
 }
+
+type INetListener interface {
+	RandomUnusedTCPPort() (int, error)
+}
+
+type NetListener struct {
+	listen func(network, address string) (net.Listener, error)
+}
+
+var _ INetListener = (*NetListener)(nil)
+
+func NewNetListener() *NetListener {
+	return &NetListener{
+		listen: net.Listen,
+	}
+}
+
+func (l *NetListener) RandomUnusedTCPPort() (int, error) {
+	addr, err := l.listen("tcp", ":0")
+	if err != nil {
+		return 0, err
+	}
+
+	if err = addr.Close(); err != nil {
+		return 0, err
+	}
+
+	return addr.Addr().(*net.TCPAddr).Port, nil
+}
--- pkg/util/net_test.go
+++ pkg/util/net_test.go
@@ -1,8 +1,11 @@
 package util
 
 import (
+	"errors"
+	"fmt"
 	"net"
 	"os"
+	"reflect"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -31,4 +34,77 @@ func TestRandomUnusedTCPPort(t *testing.T) {
 	assert.Equal(t, 999, got)
 	mockNet.AssertExpectations(t)
 	mockListener.AssertExpectations(t)
+}
+
+func TestNewNetListener(t *testing.T) {
+	l := NewNetListener()
+
+	expected, actual := reflect.ValueOf(net.Listen).Pointer(), reflect.ValueOf(l.listen).Pointer()
+
+	assert.NotNil(t, l)
+	assert.Equal(t, expected, actual)
+}
+
+func TestNetListener_RandomUnusedTCPPort(t *testing.T) {
+	type mocks struct {
+		net      *mockNet
+		listener *mockListener
+	}
+	tests := []struct {
+		mocks   *mocks
+		want    int
+		wantErr bool
+	}{
+		{
+			func() *mocks {
+				mockListener := &mockListener{}
+				mockListener.On("Close").Return(nil)
+				mockListener.On("Addr").Return(&net.TCPAddr{Port: 999}, nil)
+				mockNet := &mockNet{}
+				mockNet.On("Listen", "tcp", ":0").Return(mockListener, nil)
+				return &mocks{mockNet, mockListener}
+			}(),
+			999,
+			false,
+		},
+		{
+			func() *mocks {
+				mockListener := &mockListener{}
+				mockNet := &mockNet{}
+				mockNet.On("Listen", "tcp", ":0").Return((*net.TCPListener)(nil), errors.New("SOMETHING_WRONG"))
+				return &mocks{mockNet, mockListener}
+			}(),
+			0,
+			true,
+		},
+		{
+			func() *mocks {
+				mockListener := &mockListener{}
+				mockListener.On("Close").Return(errors.New("SOMETHING_WRONG"))
+				mockNet := &mockNet{}
+				mockNet.On("Listen", "tcp", ":0").Return(mockListener, nil)
+				return &mocks{mockNet, mockListener}
+			}(),
+			0,
+			true,
+		},
+	}
+	for i, tt := range tests {
+		t.Run(fmt.Sprintf("#%d", i), func(t *testing.T) {
+			l := &NetListener{
+				listen: tt.mocks.net.Listen,
+			}
+
+			got, err := l.RandomUnusedTCPPort()
+
+			assert.Equal(t, tt.want, got)
+			if tt.wantErr {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+			}
+			tt.mocks.net.AssertExpectations(t)
+			tt.mocks.listener.AssertExpectations(t)
+		})
+	}
 }
