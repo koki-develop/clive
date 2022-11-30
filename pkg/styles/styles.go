be74a9d4228feef0d427d3e12468292565790fa0
koki-develop <kou.pg.0131@gmail.com>
2022-12-01 08:50:02 +0900

    add styles.StyleNoteHeader

--- pkg/styles/styles.go
+++ pkg/styles/styles.go
@@ -5,16 +5,21 @@ import "github.com/charmbracelet/lipgloss"
 var (
 	// colors
 	ColorMain = lipgloss.Color("#ff00ff")
+	ColorNote = lipgloss.Color("#0000ff")
 	ColorErr  = lipgloss.Color("#ff0000")
 
 	// styles
-	StyleSpinner            = lipgloss.NewStyle().Foreground(ColorMain)
-	StyleActive             = lipgloss.NewStyle().Bold(true)
-	StyleActionHeader       = lipgloss.NewStyle().Bold(true).Padding(0, 1).Background(ColorMain)
-	StyleErrorHeader        = lipgloss.NewStyle().Bold(true).Padding(0, 1).Background(ColorErr)
-	StyleDone               = lipgloss.NewStyle().Faint(true)
-	StyleTruncated          = lipgloss.NewStyle().Faint(true)
+	StyleSpinner = lipgloss.NewStyle().Foreground(ColorMain)
+
+	StyleActionHeader = lipgloss.NewStyle().Bold(true).Padding(0, 1).Background(ColorMain)
+	StyleNoteHeader   = lipgloss.NewStyle().Bold(true).Padding(0, 1).Background(ColorNote)
+	StyleErrorHeader  = lipgloss.NewStyle().Bold(true).Padding(0, 1).Background(ColorErr)
+
 	StyleNotificationBorder = lipgloss.NewStyle().Foreground(ColorMain)
 	StyleNotificationText   = lipgloss.NewStyle().Bold(true)
-	StyleLink               = lipgloss.NewStyle().Underline(true)
+
+	StyleActive    = lipgloss.NewStyle().Bold(true)
+	StyleDone      = lipgloss.NewStyle().Faint(true)
+	StyleTruncated = lipgloss.NewStyle().Faint(true)
+	StyleLink      = lipgloss.NewStyle().Underline(true)
 )
